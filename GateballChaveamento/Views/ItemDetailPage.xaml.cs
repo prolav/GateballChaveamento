67c75574df6befe64bf35b68810504c7d02f41a5
DESKTOP-HSS81OB\Yo <viniciusryoshino@gmail.com>
2023-11-17 12:48:43 -0300

    Initial

---
+++ GateballChaveamento/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using GateballChaveamento.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace GateballChaveamento.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
